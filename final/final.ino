a098c6af9840f422c1cc96505ee110288d3be38d
Ccucumber12 <chris20021112@gmail.com>
2024-05-29 02:53:49 +0800

    [Final] arduino basic movement

--- final/final.ino
+++ final/final.ino
@@ -25,14 +25,27 @@
 #define MT2r 9
 #define PW2 10
 
+#define LEFT 1
+#define RIGHT 2
+#define FRONT 1
+#define BACK 2
 
 /***** Sensor Constants *****/
-const int WHITE_THRESH = 100;
-const int BLACK_THRESH = 500;
+const int WHITE_THRESH[4] = { 350, 300, 300, 300 };
+const int BLACK_THRESH[4] = { 770, 700, 700, 800 };
 
 #define WHITE 0
 #define BLACK 1
 #define GRAY 2
+
+
+/***** Grid Counter *****/
+
+const int STOP_NUMBER = 20;
+const int CONTINUOUS_THRESH = 3;
+int current_color = WHITE;
+int stop_count = 0;
+int change_count = 0;
 
 
 void setup() {
@@ -51,22 +64,19 @@ void setup() {
   pinMode(LED3B, OUTPUT);
   pinMode(LED4W, OUTPUT);
   pinMode(LED4B, OUTPUT);
-  
+
   // DC Motor w/ L298N control module
   pinMode(MT1l, OUTPUT);
   pinMode(MT1r, OUTPUT);
   pinMode(PW1, OUTPUT);
-  setDirection(0);
-  
+  setDirection(LEFT, FRONT);
+
   pinMode(MT2l, OUTPUT);
   pinMode(MT2r, OUTPUT);
   pinMode(PW2, OUTPUT);
-  setDirection(0);
-  
-  Serial.begin(38400);
+  setDirection(RIGHT, FRONT);
 
-  digitalWrite(LED1W, LOW);
-  digitalWrite(LED1B, LOW);
+  Serial.begin(38400);
 }
 
 
@@ -76,8 +86,205 @@ int value3;
 int value4;
 
 
+#define POWER 80
+#define TURN_POWER 200
+
 void loop() {
+  // readAllSensors();
+  // readSensor(SENSOR4, LED4W, LED4B);
+  // delay(200);
+  // drive();
+  turnLeft();
+  // handleStop();
+}
+
+
+#define GO_STRAIGHT 0
+#define TURN_LEFT 1
+#define TURN_RIGHT 2
+
+int previous_move = GO_STRAIGHT;
+
+void drive() {
   readAllSensors();
+
+  if (value1 != GRAY) {
+    turnLeft();
+  } else if (value4 != GRAY) {
+    turnRight();
+  } else {
+    if (value2 == GRAY && value3 == GRAY) {
+      if (previous_move == GO_STRAIGHT)
+        goStraight();
+      else if (previous_move == TURN_LEFT)
+        turnLeft();
+      else if (previous_move == TURN_RIGHT)
+        turnRight();
+    } else if (value3 == GRAY) {
+      turnLeft();
+    } else if (value2 == GRAY) {
+      turnRight();
+    } else {
+      goStraight();
+    }
+  }
+  // handleColor();
+  delay(10);
+}
+
+void handleColor() {
+  int new_color = getColor();
+  if (new_color != current_color) {
+    change_count += 1;
+  } else {
+    change_count = max(change_count - 1, 0);
+  }
+  if (change_count == CONTINUOUS_THRESH) {
+    change_count = 0;
+    current_color = new_color;
+    stop_count += 1;
+    if (stop_count == STOP_NUMBER) {
+      stop_count = 0;
+      handleStop();
+
+      setDirection(LEFT, FRONT);
+      setPower(LEFT, 255);
+      setDirection(RIGHT, FRONT);
+      setPower(RIGHT, 255);
+      delay(150);
+    }
+  }
+}
+
+void handleStop() {
+  setPower(LEFT, 0);
+  setPower(RIGHT, 0);
+
+  int gap = 3000 / 8 / 3;
+
+  for (int i = 0; i < 3; i++) {
+    delay(gap);
+
+    setLight(2, HIGH);
+    setLight(3, HIGH);
+    delay(gap);
+
+    setLight(1, HIGH);
+    setLight(4, HIGH);
+    delay(gap);
+
+    setLight(5, HIGH);
+    setLight(8, HIGH);
+    delay(gap);
+
+    setLight(6, HIGH);
+    setLight(7, HIGH);
+    delay(gap);
+
+    setLight(6, LOW);
+    setLight(7, LOW);
+    delay(gap);
+
+    setLight(5, LOW);
+    setLight(8, LOW);
+    delay(gap);
+
+    setLight(1, LOW);
+    setLight(4, LOW);
+    delay(gap);
+
+    setLight(2, LOW);
+    setLight(3, LOW);
+  }
+}
+
+void setLight(int index, int value) {
+  if (index == 1)
+    digitalWrite(LED4B, value);
+  if (index == 2)
+    digitalWrite(LED3B, value);
+  if (index == 3)
+    digitalWrite(LED2B, value);
+  if (index == 4)
+    digitalWrite(LED1B, value);
+  if (index == 5)
+    digitalWrite(LED4W, value);
+  if (index == 6)
+    digitalWrite(LED3W, value);
+  if (index == 7)
+    digitalWrite(LED2W, value);
+  if (index == 8)
+    digitalWrite(LED1W, value);
+}
+
+int getColor() {
+  int black_count = 0;
+  int white_count = 0;
+  black_count += (value1 == BLACK);
+  black_count += (value2 == BLACK);
+  black_count += (value3 == BLACK);
+  black_count += (value4 == BLACK);
+  white_count += (value1 == WHITE);
+  white_count += (value2 == WHITE);
+  white_count += (value3 == WHITE);
+  white_count += (value4 == WHITE);
+
+  if (black_count > white_count)
+    return WHITE;
+  if (black_count < white_count)
+    return BLACK;
+  return current_color;
+}
+
+void goStraight() {
+  setDirection(LEFT, FRONT);
+  setPower(LEFT, POWER);
+  setDirection(RIGHT, FRONT);
+  setPower(RIGHT, POWER);
+  previous_move = GO_STRAIGHT;
+}
+
+void turnLeft() {
+  setDirection(LEFT, BACK);
+  setPower(LEFT, TURN_POWER);
+  setDirection(RIGHT, FRONT);
+  setPower(RIGHT, TURN_POWER);
+  previous_move = TURN_LEFT;
+}
+
+void turnRight() {
+  setDirection(LEFT, FRONT);
+  setPower(LEFT, TURN_POWER);
+  setDirection(RIGHT, BACK);
+  setPower(RIGHT, TURN_POWER);
+  previous_move = TURN_RIGHT;
+}
+
+void runMotor() {
+  setDirection(LEFT, FRONT);
+  setDirection(RIGHT, FRONT);
+
+  analogWrite(PW1, POWER);
+  analogWrite(PW2, POWER);
+
+  delay(2000);
+
+  analogWrite(PW1, 0);
+  analogWrite(PW2, 0);
+  setDirection(LEFT, BACK);
+  setDirection(RIGHT, BACK);
+
+  delay(2000);
+
+  analogWrite(PW1, POWER);
+  analogWrite(PW2, POWER);
+
+  delay(2000);
+
+  analogWrite(PW1, 0);
+  analogWrite(PW2, 0);
+
+  delay(2000);
 }
 
 
@@ -90,17 +297,17 @@ void readAllSensors() {
 
 
 int readSensor(uint8_t sensor_pin, uint8_t white_pin, uint8_t black_pin) {
-  int value =  analogRead(sensor_pin);
-  // Serial.println(value);
+  int value = analogRead(sensor_pin);
+  Serial.println(value);
 
   int white_value = LOW;
   int black_value = LOW;
   int sensor_value = GRAY;
 
-  if (value < WHITE_THRESH) {
+  if (value < getWhiteThresh(sensor_pin)) {
     white_value = HIGH;
     sensor_value = WHITE;
-  } else if (value > BLACK_THRESH) {
+  } else if (value > getBlackThresh(sensor_pin)) {
     black_value = HIGH;
     sensor_value = BLACK;
   }
@@ -109,20 +316,95 @@ int readSensor(uint8_t sensor_pin, uint8_t white_pin, uint8_t black_pin) {
   return sensor_value;
 }
 
+int getWhiteThresh(uint8_t sensor_pin) {
+  if (sensor_pin == SENSOR1)
+    return WHITE_THRESH[0];
+  if (sensor_pin == SENSOR2)
+    return WHITE_THRESH[1];
+  if (sensor_pin == SENSOR3)
+    return WHITE_THRESH[2];
+  if (sensor_pin == SENSOR4)
+    return WHITE_THRESH[3];
+  Serial.println("[ERROR] SENSOR NOT FOUND");
+}
 
-void setDirection(int dir){
-  if (dir == 0){
-    digitalWrite(MT1l, HIGH);
-    digitalWrite(MT1r, LOW);
-  } else if (dir == 1){
-    digitalWrite(MT1l, LOW);
-    digitalWrite(MT1r, HIGH);
+int getBlackThresh(uint8_t sensor_pin) {
+  if (sensor_pin == SENSOR1)
+    return BLACK_THRESH[0];
+  if (sensor_pin == SENSOR2)
+    return BLACK_THRESH[1];
+  if (sensor_pin == SENSOR3)
+    return BLACK_THRESH[2];
+  if (sensor_pin == SENSOR4)
+    return BLACK_THRESH[3];
+  Serial.println("[ERROR] SENSOR NOT FOUND");
+}
+
+
+void setDirection(int side, int dir) {
+  if (side == LEFT) {
+    if (dir == FRONT) {
+      digitalWrite(MT1l, LOW);
+      digitalWrite(MT1r, HIGH);
+    } else if (dir == BACK) {
+      digitalWrite(MT1l, HIGH);
+      digitalWrite(MT1r, LOW);
+    } else {
+      Serial.println("[ERROR] DIRECTION NOT FOUND");
+    }
+  } else if (side == RIGHT) {
+    if (dir == FRONT) {
+      digitalWrite(MT2l, HIGH);
+      digitalWrite(MT2r, LOW);
+    } else if (dir == BACK) {
+      digitalWrite(MT2l, LOW);
+      digitalWrite(MT2r, HIGH);
+    } else {
+      Serial.println("[ERROR] DIRECTION NOT FOUND");
+    }
+  } else {
+    Serial.println("[ERROR] MOTOR NOT FOUND");
   }
 }
 
+void setPower(int direction, int power) {
+  if (direction == LEFT) {
+    analogWrite(PW1, min(255, power * 1.3));
+  } else if (direction == RIGHT) {
+    analogWrite(PW2, power);
+  } else {
+    Serial.println("[ERROR] MOTOR NOT FOUND");
+  }
+}
+
+void loopThroughLeds2() {
+  digitalWrite(LED1W, HIGH);
+  digitalWrite(LED1B, HIGH);
+  delay(1000);
+  digitalWrite(LED1B, LOW);
+  digitalWrite(LED1W, LOW);
+  digitalWrite(LED2W, HIGH);
+  digitalWrite(LED2B, HIGH);
+  delay(1000);
+  digitalWrite(LED2W, LOW);
+  digitalWrite(LED2B, LOW);
+  digitalWrite(LED3W, HIGH);
+  digitalWrite(LED3B, HIGH);
+  delay(1000);
+  digitalWrite(LED3W, LOW);
+  digitalWrite(LED3B, LOW);
+  digitalWrite(LED4W, HIGH);
+  digitalWrite(LED4B, HIGH);
+  delay(1000);
+  digitalWrite(LED4W, LOW);
+  digitalWrite(LED4B, LOW);
+  delay(1000);
+}
+
+
 void loopThroughLeds() {
   digitalWrite(LED1W, HIGH);
-  delay(1000); 
+  delay(1000);
   digitalWrite(LED1W, LOW);
   digitalWrite(LED1B, HIGH);
   delay(1000);
